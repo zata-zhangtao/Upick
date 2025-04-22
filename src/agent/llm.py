2d6aa2ff4ea4fec4600d4d562f132b3764d5fa2d
zata <1506739178@qq.com>
2025-04-22 11:06:04 +0800

    增加配置文件进行基础设置管理 feat: add configuration management and update LLM integration
    - Introduced a new configuration file (config.yaml) for application settings, including logging level, API provider, and database credentials.
    - Updated the LLM integration in llm.py to load model settings from the new configuration file, enhancing flexibility and maintainability.
    - Improved logging messages to reflect the model being used for better debugging and tracking.

--- src/agent/llm.py
+++ src/agent/llm.py
@@ -6,11 +6,20 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 import os
 from dotenv import load_dotenv
+import yaml
 from src.log import get_logger
 logger = get_logger("agent.llm")
 load_dotenv()
 
-def get_ali_llm(model="qwq-32b"):
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+yaml_path = os.path.join(ROOT_DIR, "config.yaml")
+
+with open(yaml_path, "r", encoding="utf-8") as file:
+    config_data = yaml.safe_load(file)
+
+def get_ali_llm(model):
     """
     获取阿里云的LLM模型
     Args:
@@ -28,8 +37,8 @@ def get_ali_llm(model="qwq-32b"):
     )
 
 try:
-    ali_llm = get_ali_llm("qwq-32b")
-    logger.info("阿里云模型连接成功")
+    ali_llm = get_ali_llm(model=config_data["app"]["model"])
+    logger.info(f"阿里云模型连接成功: {config_data['app']['model']}")
 except Exception as e:
     logger.error(f"阿里云模型连接失败: {e}")
     assert False, "阿里云模型连接失败"
